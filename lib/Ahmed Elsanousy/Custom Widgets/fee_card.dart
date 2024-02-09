47b7457b70d66bba26324fa310ccc76bf33796dd
AHmedElsanousy <ahmedelsanousy822@gmail.com>
2024-02-09 22:30:07 +0200

    sec commit

--- lib/Ahmed Elsanousy/Custom Widgets/fee_card.dart
+++ lib/Ahmed Elsanousy/Custom Widgets/fee_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mindcare_app/view/widgets/choose.dart';
 
 class FeeCard extends StatelessWidget {
   const FeeCard({
@@ -20,67 +22,75 @@ class FeeCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 20, right: 20, bottom: 8),
-      child: Card(
-          elevation: 2.5,
-          child: Row(
-            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12),
-                child: Container(
-                  height: 70,
-                  width: 50,
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 217, 231, 238),
-                    shape: BoxShape.circle,
+      child: InkWell(
+        onTap: () {
+          Get.bottomSheet(
+            const ChooseMethodeToPayment(),
+            backgroundColor: Colors.white,
+          );
+        },
+        child: Card(
+            elevation: 2.5,
+            child: Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12),
+                  child: Container(
+                    height: 70,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 217, 231, 238),
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(icon),
                   ),
-                  child: Icon(icon),
                 ),
-              ),
-              SizedBox(
-                width: size.width * 0.015,
-              ),
-              SizedBox(
-                width: size.width * 0.5,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(3.0),
-                      child: Text(
-                        title,
-                        style: const TextStyle(
-                            fontSize: 17, fontWeight: FontWeight.w600),
+                SizedBox(
+                  width: size.width * 0.015,
+                ),
+                SizedBox(
+                  width: size.width * 0.5,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Text(
+                          title,
+                          style: const TextStyle(
+                              fontSize: 17, fontWeight: FontWeight.w600),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(3.0),
-                      child: Text(
-                        subTitle,
-                        style: const TextStyle(
-                            color: Color.fromARGB(255, 99, 94, 94)),
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Text(
+                          subTitle,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 99, 94, 94)),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: size.width * 0.02,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "\$$price",
-                  style: const TextStyle(
-                      //color: Color.fromARGB(255, 176, 162, 36),
-                      color: Color.fromARGB(255, 55, 126, 57),
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold),
+                SizedBox(
+                  width: size.width * 0.02,
                 ),
-              ),
-            ],
-          )),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "\$$price",
+                    style: const TextStyle(
+                        //color: Color.fromARGB(255, 176, 162, 36),
+                        color: Color.fromARGB(255, 55, 126, 57),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
